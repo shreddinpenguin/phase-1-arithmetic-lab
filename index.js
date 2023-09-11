089f01b96810470cfb2e2bd6547cdec82e2f963a
Taylor Riley <t_riley13@icloud.com>
2023-09-10 21:54:44 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 62;
+const num2 = 1;
+const num3 = 12;
+const num4 = 3;
 
-// Write your code here
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random()*10)+1;
+
+const mod = num3/num4;
+
+const max = Math.max(1, 2, 3, 20)
